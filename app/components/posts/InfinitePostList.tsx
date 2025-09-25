68376a654aa32739fffc14d9250da8b10ad8047c
heinminaye <heinminaye8000@gmail.com>
2025-09-25 14:11:13 +0630

    fixedskeleongrid

--- app/components/posts/InfinitePostList.tsx
+++ app/components/posts/InfinitePostList.tsx
@@ -247,7 +247,7 @@ export default function InfinitePostList() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.5 }}
-              className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 py-5 col-span-2 "
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 py-5 col-span-2 "
             >
               {[...Array(4)].map((_, i) => (
                 <PostSkeleton key={i} />
