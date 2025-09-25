bffbc37bc120cb543dfe1265a88badbf31c75e5e
heinminaye <heinminaye8000@gmail.com>
2025-09-25 14:07:54 +0630

    fixed error for deploy

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,9 +1,16 @@
 import LoginForm from '@/components/auth/LoginForm';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-pink-900 to-purple-900">
+     <Suspense fallback={
+      <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-gray-950 flex items-center justify-center">
+        <div className="text-white">Loading...</div>
+      </div>
+    }>
       <LoginForm />
+    </Suspense>
     </div>
   );
 }
